3cf957e76c19b5ac050a25da57c027a942023057
m203882 <m203882@usna.edu>
2020-04-12 00:00:21 -0400

    I hate my life

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/index.html')
 def index():
     return render_template("index.jinja2")
 
-@app.route('/about.jinja2')
+@app.route('/about.html')
 def about():
     return render_template("about.jinja2")
 
-@app.route('/stuff.jinja2')
+@app.route('/stuff.html')
 def stuff():
     return render_template("stuff.jinja2")
+
+@app.route('/custom.html', methods=['POST'])
+def custom():
+    user_first = request.form['fname']
+    user_last = request.form['lname']
+    return 'hi %s %s' %(user_first,user_last)
