28c07ea925a86fd799af701f2cd2c160e921347e
m203882 <m203882@usna.edu>
2020-04-11 22:56:21 -0400

    I hate my life

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def main():
-    print("hello")
+from flask import Flask, render_template
+app = Flask(__name__)
 
-    if __name__ == '__main__':
-        main()
+@app.route('/index.jinja2')
+def index():
+    return render_template("index.jinja2")
+
+@app.route('/about.jinja2')
+def about():
+    return render_template("about.jinja2")
+
+@app.route('/stuff.jinja2')
+def stuff():
+    return render_template("stuff.jinja2")
